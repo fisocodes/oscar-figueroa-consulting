8e141c315ea55e4aae5586d27b1d01ebc6fbd833
Oscar Figueroa <fisocodes@gmail.com>
2025-06-14 17:17:15 -0700

    FEATURE: Added NextJS initial configuration

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
