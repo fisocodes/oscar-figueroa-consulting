61d43380a5f56ec0ff7a6ccd746cbe7340fb9e05
Oscar Figueroa <fisocodes@gmail.com>
2025-07-13 19:08:39 -0700

    FEATURE: Added Looper component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,54 +1,30 @@
 'use client'
 
 import HeroHeader from 'app/components/HeroHeader'
+import Looper from 'app/components/Looper'
 import { HeroHeaderItem } from 'messages/interfaces/HeroHeaderItem'
 import { useTranslations } from 'next-intl'
-import React, { useEffect } from 'react'
+import React from 'react'
+
+function createHeroHeader (data: HeroHeaderItem, index: number): React.ReactElement {
+  return (
+    <HeroHeader key={index}>
+      <HeroHeader.Title level={index === 0 ? 1 : 2}>{data.title}</HeroHeader.Title>
+      <HeroHeader.Description>{data.description}</HeroHeader.Description>
+    </HeroHeader>
+  )
+}
 
 export default function HomePage (): React.ReactNode {
   const t = useTranslations('home')
-
-  const heroHeaders: HeroHeaderItem[] = t.raw('heroHeader.items')
-
-  const heroHeaderComponents = heroHeaders.map((hh, i) =>
-    <HeroHeader key={i}>
-      <HeroHeader.Title level={i === 0 ? 1 : 2}>{hh.title}</HeroHeader.Title>
-      <HeroHeader.Description>{hh.description}</HeroHeader.Description>
-    </HeroHeader>
-  )
-
-  useEffect(() => {
-    if (window !== undefined && document !== undefined) {
-      const heroHeaders = Array.from(document.querySelectorAll('.hero-header'))
-
-      const titleHeroHeader = heroHeaders.at(0)
-
-      titleHeroHeader?.classList.remove('hidden')
-      titleHeroHeader?.classList.add('flex')
-
-      let heroHeaderIndex = 1
-      setInterval(() => {
-        console.log('Changing header')
-        const previousHeroHeader = heroHeaders.at(heroHeaderIndex - 1)
-        const currentHeroHeader = heroHeaders.at(heroHeaderIndex)
-
-        previousHeroHeader?.classList.remove('flex')
-        previousHeroHeader?.classList.add('hidden')
-
-        currentHeroHeader?.classList.remove('hidden')
-        currentHeroHeader?.classList.add('flex')
-
-        heroHeaderIndex++
-
-        if (heroHeaderIndex === heroHeaders.length) { heroHeaderIndex = 0 }
-      }, 6000)
-    }
-  }, [])
+  const heroHeaders = t.raw('heroHeader.items').map(createHeroHeader)
 
   return (
     <main>
       <header>
-        {heroHeaderComponents}
+        <Looper>
+          {heroHeaders}
+        </Looper>
       </header>
     </main>
   )
