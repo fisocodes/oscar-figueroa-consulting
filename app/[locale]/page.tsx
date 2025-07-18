f74a29a0b436a807efaffa0967bfa86bf1efc9b9
Oscar Figueroa <fisocodes@gmail.com>
2025-07-18 11:44:49 -0700

    FEATURE: Added footer component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Collaboration from 'app/components/Collaboration'
+import Footer from 'app/components/Footer'
 import HeroHeader from 'app/components/HeroHeader'
 import Looper from 'app/components/Looper'
 import { CollaborationsItem } from 'messages/interfaces/CollaborationsItem'
@@ -32,18 +33,24 @@ export default function HomePage (): React.ReactNode {
   const collaborations = t.raw('collaborations.items').map(createCollaboration)
 
   return (
-    <main>
+    <>
       <header className='snap-center'>
         <Looper>
           {heroHeaders}
         </Looper>
       </header>
-      <section className='snap-center h-dvh flex flex-col lg:justify-center justify-evenly items-center'>
-        <h2 className='text-4xl lg:text-6xl font-[Oxanium_Variable] text-slate-700 font-medium'>Collaborations</h2>
-        <ul className='flex grow max-h-2/3 flex-col lg:flex-row items-center lg:justify-center justify-evenly mx-10'>
-          {collaborations.map((collaboration: any, index: number) => <li key={index}>{collaboration}</li>)}
-        </ul>
-      </section>
-    </main>
+      <main>
+        <section className='snap-center h-dvh flex flex-col lg:justify-center justify-evenly items-center'>
+          <h2 className='text-4xl lg:text-6xl font-[Oxanium_Variable] text-slate-700 font-medium'>Collaborations</h2>
+          <ul className='flex grow max-h-2/3 flex-col lg:flex-row items-center lg:justify-center justify-evenly mx-10'>
+            {collaborations.map((collaboration: any, index: number) => <li key={index}>{collaboration}</li>)}
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+
   )
 }
