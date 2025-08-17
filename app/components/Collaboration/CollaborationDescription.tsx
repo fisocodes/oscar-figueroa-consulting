6ba1cdf7907bd94e336c1c15c7ed736e3c69dc80
Oscar Figueroa <fisocodes@gmail.com>
2025-08-17 14:53:05 -0700

    FEATURE: Updated Collaborations section design

--- app/components/Collaboration/CollaborationDescription.tsx
+++ app/components/Collaboration/CollaborationDescription.tsx
@@ -5,5 +5,5 @@ interface CollaborationDescriptionProps {
 }
 
 export function CollaborationDescription ({ children }: CollaborationDescriptionProps): React.ReactElement {
-  return <p className='font-light font-[Montserrat_Variable] text-xl lg:text-3xl text-center text-slate-500'>{children}</p>
+  return <p className='font-light font-[Montserrat_Variable] text-xl lg:text-2xl text-center text-slate-500'>{children}</p>
 }
