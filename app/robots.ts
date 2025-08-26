04ad76ceb89304e6bebe1fc074a668b56bb9d54f
Oscar Figueroa <fisocodes@gmail.com>
2025-08-26 12:34:45 -0700

    FEATURE: Added no index meta tag with environment variables

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,16 @@ import type { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function robots (): MetadataRoute.Robots {
+  if (process.env.ROBOTS?.includes('noindex') !== true) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/'
+      },
+      sitemap: process.env.DOMAIN
+    }
+  }
+
   return {
     rules: {
       userAgent: '*',
